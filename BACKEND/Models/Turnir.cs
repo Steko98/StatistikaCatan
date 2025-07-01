e31b1132d810bd21598a73bfdc673b6dd88cd534
Ivan Šteko <ivan.steko5@gmail.com>
2025-07-01 18:22:54 +0200

    Model klase

---
+++ BACKEND/Models/Turnir.cs
@@ -0,0 +1,9 @@
+﻿namespace BACKEND.Models
+{
+    public class Turnir : Entitet
+    {
+        public string Naziv { get; set; } = "";
+        public DateTime? DatumPocetka { get; set; }
+        public DateTime? DatumZavrsetka { get; set; }
+    }
+}
