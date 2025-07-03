5c1d8a6fa0324fe0725b262f8bef978f5b9389a3
Ivan Šteko <ivan.steko5@gmail.com>
2025-07-03 11:54:44 +0200

    Get i Post

---
+++ BACKEND/Controllers/IgracController.cs
@@ -0,0 +1,51 @@
+﻿using BACKEND.Data;
+using BACKEND.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class IgracController : ControllerBase
+    {
+        private readonly EdunovaContext _context;
+        public IgracController(EdunovaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_context.Igraci);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Igrac igrac)
+        {
+            try
+            {
+                _context.Igraci.Add(igrac);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, igrac);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ BACKEND/Models/Igrac.cs
@@ -0,0 +1,7 @@
+﻿namespace BACKEND.Models
+{
+    public class Igrac : Entitet
+    {
+        public string Ime { get; set; } = "";
+    }
+}
