d8fcf0c9df5883761da2f63643fd40102c282914
Ivan Šteko <ivan.steko5@gmail.com>
2025-07-29 18:42:24 +0200

    Add ClanService and improve error handling in services
    Introduced ClanService for managing clan-related API calls. Improved error handling in IgracService and TurnirService by returning consistent error objects and messages, and fixed some response messages for better clarity.

--- frontend/src/services/ClanService.js
+++ frontend/src/services/ClanService.js
@@ -1,37 +1,39 @@
-import { HttpService } from "./HttpService"
+import { HttpService } from "./HttpService";
 
 async function get() {
-    return await HttpService.get('/Igrac')
+    return await HttpService.get('/Clan')
     .then((odgovor)=>{
+
         return {greska: false, poruka: odgovor.data}
     })
     .catch((e)=>{
-        return {greska: true, poruka: 'Problem kod dohvaćanja igrača'}
+        return {greska: true, poruka: 'Problem kod dohvaćanja'}
     })
 }
 
+
 async function getBySifra(sifra) {
-    return await HttpService.get('/Igrac/' + sifra)
+    return await HttpService.get('/Clan/' + sifra)
     .then((odgovor)=>{
         return {greska:false, poruka: odgovor.data}
     })
-    .catch(()=>{
-        return {greska: true, poruka: 'Igrač nije pronađen'}
+    .catch((e)=>{
+        return {greska: true, poruka: 'Član nije pronađen'}
     })
 }
 
 async function obrisi(sifra) {
-    return await HttpService.delete('/Igrac/' + sifra)
+    return await HttpService.delete('/Clan/' + sifra)
     .then((odgovor)=>{
         return {greska:false, poruka: odgovor.data}
     })
     .catch(()=>{
-        return {greska: true, poruka: 'Igrač nije pronađen'}
+        return {greska: true, poruka: 'Ćlan nije pronađen'}
     })
 }
 
-async function dodaj(igrac) {
-    return await HttpService.post('/Igrac', igrac)
+async function dodaj(clan) {
+    return await HttpService.post('/Clan', clan)
     .then((odgovor)=>{
         return {greska:false, poruka: odgovor.data}
     })
@@ -45,13 +47,13 @@ async function dodaj(igrac) {
                 console.log(poruke);
                 return {greska:true, poruka: poruke}
             default:
-                return {greska: true, poruka: 'Igrač se ne može dodati'}
+                return {greska: true, poruka: 'Član se ne može dodati'}
         }
     })
 }
 
-async function promjeni(sifra, igrac) {
-    return await HttpService.put('/Igrac/' + sifra, igrac)
+async function promjeni(sifra, clan) {
+    return await HttpService.put('/Clan/' + sifra, clan)
     .then((odgovor)=>{
         return {greska: false, poruka: odgovor.data}
     })
@@ -65,7 +67,7 @@ async function promjeni(sifra, igrac) {
                 console.log(poruke);
                 return {greska:true, poruka: poruke}
             default:
-                return {greska: true, poruka: 'Igrač se ne može promjeniti'}
+                return {greska: true, poruka: 'Član se ne može promjeniti'}
         }
     })
 }
