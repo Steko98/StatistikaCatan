45d3d25d3eb26d31fb07314dead1784b82542bc5
Ivan Šteko <ivan.steko5@gmail.com>
2025-08-07 14:35:06 +0200

    Nova Get metoda
    dohvaca sve igre sa sifrom turnira, a onda sve clanove za svaku dohvacenu igru

---
+++ BACKEND/Models/DTO/DetaljiIgraDTORead.cs
@@ -0,0 +1,9 @@
+﻿namespace BACKEND.Models.DTO
+{
+    public class DetaljiIgraDTORead
+    {
+        public int Sifra { get; set; }
+        public DateTime Datum { get; set; }
+        public List<ClanDTORead> Clanovi { get; set; }
+    }
+}
---
+++ BACKEND/Models/DTO/DetaljiTurnirDTORead.cs
@@ -0,0 +1,10 @@
+﻿namespace BACKEND.Models.DTO
+{
+    public class DetaljiTurnirDTORead
+    {
+        public int Sifra { get; set; }
+        public string Naziv { get; set; }
+        public List<DetaljiIgraDTORead> Igre { get; set; }
+
+    }
+}
